5ee4b6a17c1049cf4dc781c52e16202b69517a93
Gabriella Guerra <gabriellafguerra21@gmail.com>
2025-05-17 18:40:18 -0300

    Updated database connection for compatibility

--- mod_includes/php/connect.php
+++ mod_includes/php/connect.php
@@ -3,29 +3,26 @@ include("ctracker.php");
 error_reporting(1);
 date_default_timezone_set('America/Sao_Paulo');
 
-$ip = "localhost";
-$user = "sistemae_admin"; 
+// Configuração segura do banco de dados
+$dsn = "mysql:host=localhost;dbname=sistemae_sistema;charset=utf8mb4";
+$user = "sistemae_admin";
 $senha = "infomogi123";
-$db = "sistemae_sistema";
 
-$conexao =  mysql_connect("$ip","$user","$senha");
-if($conexao)
-{       
-	if( !  mysql_select_db("$db",$conexao)  )
-	{       
-		die( mysql_error($conexao)); 	
-    }
+try {
+	$pdo = new PDO($dsn, $user, $senha, [
+		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+	]);
+} catch (PDOException $e) {
+	die("Erro ao conectar ao banco: " . $e->getMessage());
 }
-else
-{
-	die('N�o foi poss�vel conectar ao banco de dados.');     
-}
+
+// Configuração de caracteres
+$pdo->query("SET NAMES 'utf8mb4'");
+$pdo->query("SET character_set_connection=utf8mb4");
+$pdo->query("SET character_set_client=utf8mb4");
+$pdo->query("SET character_set_results=utf8mb4");
 
 include("parametros.php");
-
-
-mysql_query("SET NAMES 'utf8'");
-mysql_query('SET character_set_connection=utf8');
-mysql_query('SET character_set_client=utf8');
-mysql_query('SET character_set_results=utf8');
 ?>
+|
