977ce2cbe14fda64574774fc6e4e2aba24ad6b27
Gabriella Guerra <gabriellafguerra21@gmail.com>
2025-05-21 18:54:27 -0300

    Camada de segurança para repositório público, uso de DotEnv

--- cliente/login.php
+++ cliente/login.php
@@ -7,51 +7,51 @@ require_once '../mod_includes/php/connect.php';
 <html lang="pt">
 
 <head>
-	<title><?php echo isset($titulo) ? htmlspecialchars($titulo, ENT_QUOTES, 'UTF-8') : 'Login'; ?></title>
-	<meta name="author" content="MogiComp">
-	<meta charset="UTF-8">
-	<link rel="shortcut icon" href="../imagens/favicon.png">
-	<?php require_once '../css/style.php'; ?>
-	<script src="../mod_includes/js/jquery-1.8.3.min.js"></script>
+    <title><?php echo isset($titulo) ? htmlspecialchars($titulo, ENT_QUOTES, 'UTF-8') : 'Login'; ?></title>
+    <meta name="author" content="MogiComp">
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="../imagens/favicon.png">
+    <?php require_once '../css/style.php'; ?>
+    <script src="../mod_includes/js/jquery-1.8.3.min.js"></script>
 </head>
 
 <body>
-	<?php
+    <?php
 	require_once '../mod_includes/php/funcoes-jquery.php';
 	require_once '../mod_topo_cliente/topo_login.php';
 	?>
 
-	<main class="centro">
-		<h1 class="titulo">Bem-vindo ao Sistema de Gerenciamento de Orçamentos</h1>
-		<section id="interna">
-			<form name="form_login" id="form_login" method="post" autocomplete="off" action="envialogin.php">
-				<table align="center" cellspacing="10">
-					<tr>
-						<td>
-							<span class="textopeq">Digite seu usuário e senha para acessar o sistema.</span>
-						</td>
-					</tr>
-					<tr>
-						<td align="center">
-							<input type="email" name="login" id="login" placeholder="Email" required size="20">
-						</td>
-					</tr>
-					<tr>
-						<td align="center">
-							<input type="password" name="senha" id="senha" placeholder="Senha" required size="20">
-						</td>
-					</tr>
-					<tr>
-						<td align="center" height="30" valign="bottom">
-							<button type="submit" id="bt_login" name="B1">Entrar no Sistema</button>
-						</td>
-					</tr>
-				</table>
-			</form>
-		</section>
-	</main>
+    <main class="centro">
+        <h1 class="titulo">Bem-vindo ao Sistema de Gerenciamento de Orçamentos</h1>
+        <section id="interna">
+            <form name="form_login" id="form_login" method="post" autocomplete="off" action="envialogin.php">
+                <table align="center" cellspacing="10">
+                    <tr>
+                        <td>
+                            <span class="textopeq">Digite seu usuário e senha para acessar o sistema.</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="center">
+                            <input type="email" name="login" id="login" placeholder="Email" required size="20">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="center">
+                            <input type="password" name="senha" id="senha" placeholder="Senha" required size="20">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="center" height="30" valign="bottom">
+                            <input type="submit" id="bt_login" name="B1" value="Entrar no Sistema">
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </section>
+    </main>
 
-	<?php require_once '../mod_rodape/rodape.php'; ?>
+    <?php require_once '../mod_rodape/rodape.php'; ?>
 </body>
 
 </html>
--- mod_includes/php/connect.php
+++ mod_includes/php/connect.php
@@ -1,13 +1,20 @@
 <?php
+use Dotenv\Dotenv;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
+
 include 'ctracker.php';
 
 error_reporting(1);
 date_default_timezone_set('America/Sao_Paulo');
 
-// Configuração segura do banco de dados
-$dsn = 'mysql:host=localhost;dbname=sistemae_sistema;charset=utf8mb4';
-$user = 'sistemae_admin';
-$password = 'infomogi123';
+// Use o DSN diretamente do .env
+$dsn = $_ENV['DB_DSN'];
+$user = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
 
 try {
 	$pdo = new PDO($dsn, $user, $password, [
